d2baa0a7497d4ad2e2b325de58ac843e4e76032a
Brad Misik <temugen@bradmisik.com>
2012-02-15 18:49:37 -0600

    Added logic unit test target

---
+++ shiftLogicTests/shiftLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  shiftLogicTests.h
+//  shiftLogicTests
+//
+//  Created by Brad Misik on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface shiftLogicTests : SenTestCase
+
+@end
---
+++ shiftLogicTests/shiftLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  shiftLogicTests.m
+//  shiftLogicTests
+//
+//  Created by Brad Misik on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "shiftLogicTests.h"
+
+@implementation shiftLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //STFail(@"Unit tests are not implemented yet in shiftLogicTests");
+}
+
+@end
