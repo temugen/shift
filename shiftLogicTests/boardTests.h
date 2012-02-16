c210b1b56c348a3a073de787ca111894299d07b1
Brad Misik <temugen@bradmisik.com>
2012-02-15 19:03:36 -0600

    Added boardTests

---
+++ shiftLogicTests/boardTests.h
@@ -0,0 +1,13 @@
+//
+//  boardTests.h
+//  shift
+//
+//  Created by Brad Misik on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface boardTests : SenTestCase
+
+@end
---
+++ shiftLogicTests/boardTests.m
@@ -0,0 +1,32 @@
+//
+//  boardTests.m
+//  shift
+//
+//  Created by Brad Misik on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "boardTests.h"
+
+@implementation boardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //STFail(@"Unit tests are not implemented yet in shiftLogicTests");
+}
+
+@end
