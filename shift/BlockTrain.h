b17f12dcd15ca6b7804949d3864b0b48abba68b9
Brad Misik <temugen@bradmisik.com>
2012-02-28 00:20:03 -0600

    Moved control layer to src folder, added blocktrain

---
+++ shift/BlockTrain.h
@@ -0,0 +1,13 @@
+//
+//  BlockTrain.h
+//  shift
+//
+//  Created by Brad Misik on 2/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockTrain : NSObject
+
+@end
---
+++ shift/BlockTrain.m
@@ -0,0 +1,13 @@
+//
+//  BlockTrain.m
+//  shift
+//
+//  Created by Brad Misik on 2/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BlockTrain.h"
+
+@implementation BlockTrain
+
+@end
