1efcb78b47e42497801f4c73346ab78ff3ade519
Alex Chesebro <arpchesebro@gmail.com>
2012-03-05 14:25:51 -0600

    Started adding achievement functions to process and send data back and forth between GameCenter

---
+++ shift/GKAchievementViewController-LandscapeOnly.h
@@ -0,0 +1,13 @@
+//
+//  GKAchievementViewController-LandscapeOnly.h
+//  shift
+//
+//  Created by Alex Chesebro on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GKAchievementViewController_LandscapeOnly : NSObject
+
+@end
---
+++ shift/GKAchievementViewController-LandscapeOnly.m
@@ -0,0 +1,13 @@
+//
+//  GKAchievementViewController-LandscapeOnly.m
+//  shift
+//
+//  Created by Alex Chesebro on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GKAchievementViewController-LandscapeOnly.h"
+
+@implementation GKAchievementViewController_LandscapeOnly
+
+@end
