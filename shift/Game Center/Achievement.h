c934195f3bf76182c1829690a2047fb15aab6881
Jicong Wang <wjicong@gmail.com>
2012-03-22 14:24:05 -0500

    added achievements encoding and decoding

---
+++ shift/Game Center/Achievement.h
@@ -0,0 +1,13 @@
+//
+//  Achievement.h
+//  shift
+//
+//  Created by Jicong Wang on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Achievement : NSObject
+
+@end
---
+++ shift/Game Center/Achievement.m
@@ -0,0 +1,13 @@
+//
+//  Achievement.m
+//  shift
+//
+//  Created by Jicong Wang on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Achievement.h"
+
+@implementation Achievement
+
+@end
