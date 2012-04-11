86a34b2ed9df7890dbdf16f109e2b4bce0b1ea55
Greg McLain <mclain.greg@gmail.com>
2012-04-11 18:51:20 -0500

    Added in game menu files

---
+++ shift/Menus/QuickplayGameMenu.m
@@ -0,0 +1,13 @@
+//
+//  QuickplayGameMenu.m
+//  shift
+//
+//  Created by Greg McLain on 4/11/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "QuickplayGameMenu.h"
+
+@implementation QuickplayGameMenu
+
+@end
---
+++ shift/Menus/SinglePlayerGameMenu.h
@@ -0,0 +1,13 @@
+//
+//  SinglePlayerGameMenu.h
+//  shift
+//
+//  Created by Greg McLain on 4/11/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "InGameMenu.h"
+
+@interface SinglePlayerGameMenu : InGameMenu
+
+@end
