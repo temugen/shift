395e80182db32fdb92d505626cf85f4d13d2df25
Greg McLain <mclain.greg@gmail.com>
2012-04-16 12:47:37 -0500

    Add "button press" to level select.

--- shift/Menus/SinglePlayerMenu.h
+++ shift/Menus/SinglePlayerMenu.h
@@ -6,8 +6,12 @@
 //  Copyright (c) 2012. All rights reserved.
 //
 
-#import "cocos2d.h"
 #import "Menu.h"
+
+#define SPRITES_PER_PAGE 4
+#define PADDING 40
+#define LOCKED -1
+#define LEVEL_TEXTURE -1
 
 @interface SinglePlayerMenu : Menu
 {
