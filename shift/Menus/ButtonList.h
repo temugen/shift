fde5afd8ed65a3a001c3f5a3309b6905ea919074
Brad Misik <temugen@bradmisik.com>
2012-04-18 18:04:31 -0500

    Add ButtonList class for menus

---
+++ shift/Menus/ButtonList.h
@@ -0,0 +1,13 @@
+//
+//  ButtonList.h
+//  shift
+//
+//  Created by Brad Misik on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ButtonList : NSObject
+
+@end
---
+++ shift/Menus/ButtonList.m
@@ -0,0 +1,13 @@
+//
+//  ButtonList.m
+//  shift
+//
+//  Created by Brad Misik on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ButtonList.h"
+
+@implementation ButtonList
+
+@end
