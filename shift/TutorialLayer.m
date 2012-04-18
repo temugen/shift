9c6c4b33e146d03f816d90e99954c967b6d1a21d
Brad Misik <temugen@bradmisik.com>
2012-04-18 00:13:52 -0500

    Tutorials now slide in and out of view

--- shift/TutorialLayer.m
+++ shift/TutorialLayer.m
@@ -9,6 +9,7 @@
 #import "TutorialLayer.h"
 #import "Tutorial.h"
 #import "CellSprite.h"
+#import "RoundedRectangle.h"
 
 @implementation TutorialLayer
 
@@ -37,7 +38,16 @@
 
 -(void)displayNext
 {
-    [self removeAllChildrenWithCleanup:YES];
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    
+    //Slide out of window
+    for (CCNode *child in self.children) {
+        CGPoint offScreen = ccp(child.position.x, -screenSize.height / 2);
+        id actionMove = [CCMoveTo actionWithDuration:1.5 position:offScreen];
+        id actionRemove = [CCCallFuncND actionWithTarget:child selector:@selector(removeFromParentAndCleanup:) data:(void *)YES];
+        id actionSequence = [CCSequence actions:actionMove, actionRemove, nil];
+        [child runAction:actionSequence];
+    }
     
     if ([tutorials count] <= 0) {
         return;
@@ -47,13 +57,30 @@
     [tutorials removeObjectAtIndex:0];
     
     CellSprite *cell = [tutorial.cell copy];
-    cell.position = ccp(CGRectGetWidth(cell.boundingBox) / 2, CGRectGetHeight(cell.boundingBox) / 2);
     [self addChild:cell];
     
     CCLabelTTF *message = [CCLabelTTF labelWithString:tutorial.message fontName:@"Helvetica" fontSize:20];
-    message.position = ccp(CGRectGetWidth(cell.boundingBox) + message.contentSize.width / 2,
-                           message.contentSize.height / 2);
     [self addChild:message];
+    
+    RoundedRectangle *bg = [[RoundedRectangle alloc] initWithWidth:screenSize.width - platformPadding
+                                                           height:CGRectGetHeight(cell.boundingBox) + (2 * platformPadding)
+                                                          pressed:NO];
+    [self addChild:bg z:-1];
+    
+    bg.position = ccp(screenSize.width / 2, bg.contentSize.height / 2);
+    cell.position = ccp(CGRectGetMinX(bg.boundingBox) + platformPadding + CGRectGetWidth(cell.boundingBox) / 2,
+                        CGRectGetMinY(bg.boundingBox) + platformPadding + CGRectGetHeight(cell.boundingBox) / 2);
+    message.position = ccp(CGRectGetMaxX(cell.boundingBox) + platformPadding + message.contentSize.width / 2,
+                           CGRectGetMinY(bg.boundingBox) + platformPadding + message.contentSize.height / 2);
+    
+    //Should put all of this in a single node to move it at once
+    //Slide in to window
+    float yDiff = -bg.contentSize.height / 2;
+    for (CCNode *child in self.children) {
+        id actionMove = [CCMoveTo actionWithDuration:1.0 position:child.position];
+        child.position = ccp(child.position.x, yDiff);
+        [child runAction:actionMove];
+    }
 }
 
 -(void) onNewTutorial:(NSNotification *)notification
