3ecae16f3845cf2341a0840bced0e04249eb37bf
Jicong Wang <wjicong@gmail.com>
2012-04-18 19:46:30 -0500

    added sound button in the option menu, but need to change the position and icon

--- shift/Menus/OptionsMenu.m
+++ shift/Menus/OptionsMenu.m
@@ -15,8 +15,33 @@
 {
     if( (self=[super init] )) {
         [self addBackButton];
+        [self addSoundButton];
     }
+    
     return self;
 }
 
+-(void)soundSwitch:(id)sender
+{
+    if ([[SimpleAudioEngine sharedEngine] mute]) {
+        // This will unmute the sound
+        [[SimpleAudioEngine sharedEngine] setMute:0];
+    }
+    else {
+        //This will mute the sound
+        [[SimpleAudioEngine sharedEngine] setMute:1];
+    }
+}
+-(void)addSoundButton{
+
+    _plusItem = [CCMenuItemImage itemFromNormalImage:@"Icon.png" 
+                                    selectedImage:@"Icon.png" target:nil selector:nil];
+    _minusItem = [CCMenuItemImage itemFromNormalImage:@"Icon.png" 
+                                     selectedImage:@"Icon.png" target:nil selector:nil];
+    CCMenuItemToggle *toggleItem = [CCMenuItemToggle itemWithTarget:self 
+                                                           selector:@selector(soundSwitch:) items:_plusItem, _minusItem, nil];
+    CCMenu *toggleMenu = [CCMenu menuWithItems:toggleItem, nil];
+    toggleMenu.position = ccp(60, 120);
+    [self addChild:toggleMenu];
+}
 @end
