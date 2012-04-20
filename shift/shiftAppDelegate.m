a7c5e549f4e6832222bdfe6ab4f023461dee72a4
Brad Misik <temugen@bradmisik.com>
2012-04-20 17:08:50 -0500

    Isolate platform initialization

--- shift/shiftAppDelegate.m
+++ shift/shiftAppDelegate.m
@@ -48,6 +48,22 @@
   return viewController;
 }
 
+-(void) initPlatformVariables
+{
+    platformPadding = 10;
+    
+    //Set cell size
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    GoalSprite *sampleGoal = [GoalSprite goalWithName:@"red"];
+    float sampleSize = CGRectGetWidth([sampleGoal boundingBox]);
+    float requestedCellSize = MIN(sampleSize, (screenSize.height - platformPadding * 2) / kDifficultyHard);
+    platformCellSize = CGSizeMake(requestedCellSize, requestedCellSize);
+
+    platformFontSize = 3 * platformPadding;
+    platformMinCollisionForce = platformCellSize.width;
+    platformDirectionThreshold = 4;
+}
+
 - (void) applicationDidFinishLaunching:(UIApplication*)application
 {
 	// Init the window
@@ -133,19 +149,8 @@
     [CCFileUtils setiPadSuffix:@"~ipad"];
     [CCFileUtils setiPhoneRetinaDisplaySuffix:@"@2x"];
     [CCFileUtils setiPadRetinaDisplaySuffix:@"@2x~ipad"];
-
-    //Set cell size for platform
-    CGSize screenSize = [director winSize];
-    GoalSprite *sampleGoal = [GoalSprite goalWithName:@"red"];
-    float sampleSize = CGRectGetWidth([sampleGoal boundingBox]);
-    float requestedCellSize = MIN(sampleSize, (screenSize.height - platformPadding * 2) / kDifficultyHard);
-    platformCellSize = CGSizeMake(requestedCellSize, requestedCellSize);
     
-    //Set platform variables
-    platformPadding = 10;
-    platformFontSize = 3 * platformPadding;
-    platformMinCollisionForce = platformCellSize.width;
-    platformDirectionThreshold = 4;
+    [self initPlatformVariables];
     
 	// Run the intro Scene
 	[[CCDirector sharedDirector] runWithScene: [MainMenu scene]];
