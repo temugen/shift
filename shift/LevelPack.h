77407266ca1ea3e29d2b64efb25e004f5fa418f0
Brad Misik <temugen@bradmisik.com>
2012-04-20 18:24:27 -0500

    Add LevelPack to map level names to numbers

---
+++ shift/LevelPack.h
@@ -0,0 +1,13 @@
+//
+//  LevelPack.h
+//  shift
+//
+//  Created by Brad Misik on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelPack : NSObject
+
+@end
---
+++ shift/LevelPack.m
@@ -0,0 +1,13 @@
+//
+//  LevelPack.m
+//  shift
+//
+//  Created by Brad Misik on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LevelPack.h"
+
+@implementation LevelPack
+
+@end
