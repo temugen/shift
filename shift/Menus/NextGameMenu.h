384648d27f906d060f465f94fb3b40253fe9d7ba
Brad Misik <temugen@bradmisik.com>
2012-04-29 14:23:52 -0500

    Add NextGameMenu

---
+++ shift/Menus/NextGameMenu.h
@@ -0,0 +1,13 @@
+//
+//  NextGameMenu.h
+//  shift
+//
+//  Created by Brad Misik on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NextGameMenu : NSObject
+
+@end
---
+++ shift/Menus/NextGameMenu.m
@@ -0,0 +1,13 @@
+//
+//  NextGameMenu.m
+//  shift
+//
+//  Created by Brad Misik on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NextGameMenu.h"
+
+@implementation NextGameMenu
+
+@end
